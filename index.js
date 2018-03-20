6c63079bd1bf525469b61b25dc00dd339b16ffa8
Grigoras Nicu <grigoras.nic@gmail.com>
2018-03-20 15:45:49 +0200

    change a few lines for security

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/', function (req, res) {
 
 
 app.listen(app.get('port'), function() {
-    console.log('MGM Platform App is running on port', app.get('port'));
+    console.log('Node App is running on port', app.get('port'));
 });
 
 
